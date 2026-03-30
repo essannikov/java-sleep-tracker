db9b923417be3913794ae0804809242b27d353ed
Evgenii Sannikov <essannikov@gmail.com>
2026-03-30 21:50:21 +0300

    Fix CheckStyle. Unused import

--- src/main/java/ru/yandex/practicum/sleeptracker/enums/UserClassification.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/enums/UserClassification.java
@@ -1,8 +1,6 @@
 package ru.yandex.practicum.sleeptracker.enums;
 
 import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Optional;
 
 public enum UserClassification {
